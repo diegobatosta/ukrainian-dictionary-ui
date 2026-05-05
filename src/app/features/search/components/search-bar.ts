e1736008ed2dcce9a120ec95989e93251d001b2d
Diogo Batista <diego.batosta@outlook.com>
2026-05-05 19:11:40 +0100

    feat: add search functionality
    Add search functionality, which consists primarily of a search bar and a
    button, as well as a grid of matches or an error message otherwise.
    Additionally, the page displays a loading disk while the frontend waits
    for the backend's response.

---
+++ src/app/features/search/components/search-bar.ts
@@ -0,0 +1,42 @@
+import { Component, computed, inject, signal } from '@angular/core';
+import { rxResource } from '@angular/core/rxjs-interop';
+import { RouterLink } from '@angular/router';
+
+import { of } from 'rxjs';
+
+import { EntryService } from '../../../core/services/entry-service';
+
+@Component({
+  selector: 'app-search-bar',
+  imports: [RouterLink],
+  templateUrl: './search-bar.html',
+  styleUrl: './search-bar.css',
+})
+export class SearchBar {
+  readonly service = inject(EntryService);
+
+  query = signal('');
+  submittedQuery = signal('');
+
+  isLoading = computed(
+    () =>
+      this.query().trim().length === 0 ||
+      (this.results.isLoading() && this.submittedQuery().trim().length > 0),
+  );
+
+  results = rxResource({
+    params: () => this.submittedQuery(),
+    stream: ({ params: q }) => {
+      if (!q.trim()) return of([]);
+      return this.service.search(q);
+    },
+  });
+
+  onInput(event: Event): void {
+    this.query.set((event.target as HTMLInputElement).value);
+  }
+
+  onSearch(): void {
+    this.submittedQuery.set(this.query());
+  }
+}
