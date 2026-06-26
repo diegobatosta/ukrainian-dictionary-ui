0656fb7115dad294d5bd76f42e70ba9e4487ceec
Diogo Batista <diego.batosta@outlook.com>
2026-06-26 12:53:47 +0100

    fix: fix broken tests
    Fix two unit tests that are broken. One is an app test which is looking
    for a heading that has a value that is no longer valid; the other has a
    property that must not be null, so it is mocked instead.
    
    These tests have been broken for some time, but the addition of a
    workflow that runs the tests automatically (see commit fbb31c6) now
    makes sure that this will not happen again.

--- src/app/features/entries/components/entry-page.spec.ts
+++ src/app/features/entries/components/entry-page.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { provideRouter, RouterModule } from '@angular/router';
 
 import { EntryPage } from './entry-page';
 
@@ -9,10 +10,18 @@ describe('EntryPage', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [EntryPage],
+      providers: [provideRouter([])],
     }).compileComponents();
 
     fixture = TestBed.createComponent(EntryPage);
     component = fixture.componentInstance;
+    component.entry.set({
+      slug: 'pryvit',
+      ukrainian: 'привіт',
+      russian: 'привет',
+      english: 'hello',
+    });
+
     await fixture.whenStable();
   });
 
